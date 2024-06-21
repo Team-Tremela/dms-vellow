cf76f5786f7d214cd8a97404f35f4f54348889c7
Chiranjibi Sahu <chiranjibi098@gmail.com>
2024-06-21 10:13:02 +0530

    logout function in user popover

--- src/layouts/dashboard/common/account-popover.jsx
+++ src/layouts/dashboard/common/account-popover.jsx
@@ -9,6 +9,8 @@ import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 
+import { useRouter } from 'src/routes/hooks';
+
 import { account } from 'src/_mock/account';
 
 // ----------------------------------------------------------------------
@@ -32,6 +34,10 @@ const MENU_OPTIONS = [
 
 export default function AccountPopover() {
   const [open, setOpen] = useState(null);
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/');
+  };
 
   const handleOpen = (event) => {
     setOpen(event.currentTarget);
@@ -105,7 +111,7 @@ export default function AccountPopover() {
         <MenuItem
           disableRipple
           disableTouchRipple
-          onClick={handleClose}
+          onClick={handleClick}
           sx={{ typography: 'body2', color: 'error.main', py: 1.5 }}
         >
           Logout
