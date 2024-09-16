ba8629975efdac9f06543812b8e0991a9cee4473
Chiranjibi Sahu <chiranjibi098@gmail.com>
2024-09-16 12:26:46 +0530

    Download barcode fixed

--- src/sections/vechicles/vechicles-table-toolbar.jsx
+++ src/sections/vechicles/vechicles-table-toolbar.jsx
@@ -1,9 +1,10 @@
 // import * as XLSX from 'xlsx';
 import ExcelJS from 'exceljs';
+import JsBarcode from 'jsbarcode';
 import PropTypes from 'prop-types';
-import Barcode from 'react-barcode';
+// import Barcode from 'react-barcode';
 import { toast } from 'react-hot-toast';
-import { renderToStaticMarkup } from 'react-dom/server';
+// import { renderToStaticMarkup } from 'react-dom/server';
 
 import Tooltip from '@mui/material/Tooltip';
 import Toolbar from '@mui/material/Toolbar';
@@ -88,42 +89,29 @@ export default function VechiclesTableToolbar({
 
   console.log(tableData);
 
-  const generateBarcodeImage = async (value) => {
-    try {
-      // Render the barcode SVG
-      const barcodeSvg = renderToStaticMarkup(<Barcode value={value} />);
-
-      if (!barcodeSvg || !barcodeSvg.includes('<svg')) {
-        throw new Error('Invalid SVG content generated');
-      }
-
-      // Encode the SVG to base64
-      const encodedSvg = btoa(barcodeSvg);
-      console.log(barcodeSvg);
-
+  const generateBarcodeImage = (value) =>
+    new Promise((resolve, reject) => {
+      // Create a temporary canvas element
       const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      const img = new Image();
-      img.src = `data:image/svg+xml;base64,${encodedSvg}`;
-      console.log(img);
-      return new Promise((resolve, reject) => {
-        img.onload = () => {
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img, 0, 0);
-          const dataUrl = canvas.toDataURL('image/png');
-          resolve(dataUrl);
-        };
 
-        img.onerror = (error) => {
-          reject(new Error(`Error loading image: ${error.message}`));
-        };
-      });
-    } catch (error) {
-      console.error('Error generating barcode image:', error);
-      throw error;
-    }
-  };
+      try {
+        JsBarcode(canvas, value, {
+          format: 'CODE128',
+          height: 30, // Adjust height here
+          width: 4,
+          displayValue: true, // Enable displaying the value below the barcode
+          fontSize: 24, // Adjust the font size as needed
+          textMargin: 10, // Margin between the barcode and the text
+        });
+
+        // Convert canvas to data URL
+        const dataUrl = canvas.toDataURL('image/png');
+        resolve(dataUrl);
+      } catch (error) {
+        console.error('Error generating barcode image:', error);
+        reject(error);
+      }
+    });
 
   const handleDownloadExcel = async () => {
     console.log('Starting to download Excel');
@@ -131,7 +119,16 @@ export default function VechiclesTableToolbar({
     const workbook = new ExcelJS.Workbook();
     const worksheet = workbook.addWorksheet('Vehicles');
 
-    worksheet.addRow([
+    // Define center alignment style
+    const centerAlignStyle = {
+      alignment: {
+        vertical: 'middle',
+        horizontal: 'center',
+      },
+    };
+
+    // Add headers
+    const headerRow = worksheet.addRow([
       'ID',
       'Vehicle ID',
       'Model Name',
@@ -148,7 +145,39 @@ export default function VechiclesTableToolbar({
       'Barcode',
     ]);
 
+    // Apply center alignment to header row
+    headerRow.eachCell({ includeEmpty: true }, (cell) => {
+      cell.style = centerAlignStyle;
+    });
+
     console.log('Headers added to worksheet');
+
+    // Set column width and row height
+    worksheet.columns = [
+      { width: 20 }, // Adjust widths as needed for other columns
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 20 },
+      { width: 40 },
+      { width: 40 },
+    ];
+
+    worksheet.eachRow({ includeEmpty: true }, (row, rowIndex) => {
+      if (rowIndex > 1) {
+        // Skip header row
+        worksheet.getRow(rowIndex).height = 200; // Set height for each row
+
+        // Apply center alignment style to each cell
+        row.eachCell(centerAlignStyle);
+      }
+    });
 
     try {
       const barcodeImages = await Promise.all(
@@ -166,21 +195,24 @@ export default function VechiclesTableToolbar({
       console.log('Barcode images generated');
 
       tableData.forEach((item, index) => {
-        worksheet.addRow([
-          item.id,
-          item.vehicle_id,
-          item.model_name,
-          item.vin,
-          item.dealer_id,
-          item.chassis_no,
-          item.motor_no,
-          item.battery_no,
-          item.batch_no,
-          item.color_code,
-          item.mfg_date,
-          item.createdAt,
-          item.updatedAt,
-        ]);
+        worksheet
+          .addRow([
+            item.id,
+            item.vehicle_id,
+            item.model_name,
+            item.vin,
+            item.dealer_id,
+            item.chassis_no,
+            item.motor_no,
+            item.battery_no,
+            item.batch_no,
+            item.color_code,
+            item.mfg_date,
+            item.createdAt,
+            item.updatedAt,
+          ]).eachCell({ includeEmpty: true }, (cell) => {
+            cell.style = centerAlignStyle;
+          }); // Apply center alignment to each row
 
         const barcodeImage = barcodeImages.find((img) => img.id === item.id);
 
@@ -190,10 +222,21 @@ export default function VechiclesTableToolbar({
             base64: barcodeImage.barcodeImage,
             extension: 'png',
           });
+
+          const rowIndex = index + 1; // Since row index starts from 2 (1 for header)
+          const rowIndexs = index + 2; // Since row index starts from 2 (1 for header)
+          console.log(rowIndex);
+
+          // Calculate the cell where the image should be placed
           worksheet.addImage(imageId, {
-            tl: { col: 13, row: index + 2 },
-            ext: { width: 150, height: 60 },
+            tl: { col: 13, row: rowIndex }, // Top-left corner of image (cell B2 is col 1, row 1)
+            ext: { width: 200, height: 100 }, // Adjust image dimensions to fit within cell
           });
+
+          // Adjust column width and row height to match the image size
+          worksheet.getColumn(13).width = 30; // Width of column B (Adjust as needed)
+          worksheet.getColumn(14).width = 30; // Width of column B (Adjust as needed)
+          worksheet.getRow(rowIndexs).height = 100; // Height of row 2 (Adjust as needed)
         }
       });
 
@@ -219,6 +262,134 @@ export default function VechiclesTableToolbar({
       toast.error('Failed to download the file. Please try again.');
     }
   };
+
+  // const handleDownloadExcel = async () => {
+  //   console.log('Starting to download Excel');
+
+  //   const workbook = new ExcelJS.Workbook();
+  //   const worksheet = workbook.addWorksheet('Vehicles');
+
+  //   // Add headers
+  //   worksheet.addRow([
+  //     'ID',
+  //     'Vehicle ID',
+  //     'Model Name',
+  //     'VIN',
+  //     'Dealer ID',
+  //     'Chassis No',
+  //     'Motor No',
+  //     'Battery No',
+  //     'Batch No',
+  //     'Color Code',
+  //     'MFG Date',
+  //     'Created At',
+  //     'Updated At',
+  //     'Barcode',
+  //   ]);
+
+  //   console.log('Headers added to worksheet');
+  //   // Set column width and row height
+  //   worksheet.columns = [
+  //     { width: 20 }, // Adjust widths as needed for other columns
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 20 },
+  //     { width: 40 },
+  //     { width: 40 },
+  //   ];
+
+  //   try {
+  //     const barcodeImages = await Promise.all(
+  //       tableData.map(async (item) => {
+  //         if (item.barcodeImage) {
+  //           return { id: item.id, barcodeImage: item.barcodeImage };
+  //         }
+  //         console.log(item.barcode);
+  //         console.log(`Generating barcode for barcode: ${item.barcode}`);
+  //         const barcodeImage = await generateBarcodeImage(item.barcode);
+  //         return { id: item.id, barcodeImage };
+  //       })
+  //     );
+
+  //     console.log('Barcode images generated');
+
+  //     tableData.forEach((item, index) => {
+  //       worksheet.addRow([
+  //         item.id,
+  //         item.vehicle_id,
+  //         item.model_name,
+  //         item.vin,
+  //         item.dealer_id,
+  //         item.chassis_no,
+  //         item.motor_no,
+  //         item.battery_no,
+  //         item.batch_no,
+  //         item.color_code,
+  //         item.mfg_date,
+  //         item.createdAt,
+  //         item.updatedAt,
+  //       ]);
+
+  //       const barcodeImage = barcodeImages.find((img) => img.id === item.id);
+
+  //       if (barcodeImage) {
+  //         console.log(`Adding barcode for ID: ${item.id}`);
+  //         const imageId = workbook.addImage({
+  //           base64: barcodeImage.barcodeImage,
+  //           extension: 'png',
+  //         });
+
+  //         const rowIndex = index + 1; // Since row index starts from 2 (1 for header)
+  //         const rowIndexs = index + 2; // Since row index starts from 2 (1 for header)
+  //         console.log(rowIndex);
+
+  //         // Calculate the cell where the image should be placed
+  //         worksheet.addImage(imageId, {
+  //           tl: { col: 13, row: rowIndex }, // Top-left corner of image (cell B2 is col 1, row 1)
+  //           ext: { width: 100, height: 100 }, // Adjust image dimensions to fit within cell
+  //         });
+
+  //         // Adjust column width and row height to match the image size
+  //         worksheet.getColumn(13).width = 30; // Width of column B (Adjust as needed)
+  //         worksheet.getColumn(14).width = 30; // Width of column B (Adjust as needed)
+  //         worksheet.getRow(rowIndexs).height = 100; // Height of row 2 (Adjust as needed)
+
+  //         // worksheet.addImage(imageId, {
+  //         //   tl: { col: 13, row: rowIndex }, // Column N is 13 (0-indexed), dynamic row
+  //         //   ext: { width: 150, height: 60 },
+  //         // });
+  //       }
+  //     });
+
+  //     console.log('Rows and barcode images added to worksheet');
+
+  //     const buffer = await workbook.xlsx.writeBuffer();
+  //     const blob = new Blob([buffer], {
+  //       type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  //     });
+
+  //     const link = document.createElement('a');
+  //     link.href = URL.createObjectURL(blob);
+  //     link.download = `Vehicles_${formatDateTime()}.xlsx`.replace(/ /g, '_');
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+
+  //     console.log('File download triggered');
+
+  //     toast.success('File downloaded successfully!');
+  //   } catch (error) {
+  //     console.error('Error during Excel download process:', error); // Debug: Catch and log errors
+  //     toast.error('Failed to download the file. Please try again.');
+  //   }
+  // };
 
   return (
     <Toolbar
